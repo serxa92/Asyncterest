7ca5e3db472ab8b479fba93bc890660d833725f7
Sergio <serxa92@hotmail.com>
2025-04-03 13:00:48 +0200

    Fix: Actualizar rutas de imágenes en el componente Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,12 +6,12 @@ export function Sidebar() {
       
       <nav>
         <ul>
-          <li><a href="#" data-label="Home"><img class="logo" src="public/logo.png" alt="Logo" /></a></li>
-          <li><a href="#" data-label="Search"><img class="logos" src="public/busqueda (1).png" alt="Logo" /></a></li>
-          <li><a href="#" data-label="Create"><img class="logos" src="public/mas (1).png" alt="Logo" /></a></li>
-          <li><a href="#" data-label="Notifications"><img class="logos" src="public/campana.png" alt="Logo" /></a></li>
-          <li><a href="#" data-label="Messages"><img class="logos" src="public/sobre.png" alt="Logo" /></a></li>
-          <li><a href="#" data-label="Settings"><img class="logos" src="public/ajustes.png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Home"><img class="logo" src="/logo.png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Search"><img class="logos" src="/busqueda (1).png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Create"><img class="logos" src="/mas (1).png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Notifications"><img class="logos" src="/campana.png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Messages"><img class="logos" src="/sobre.png" alt="Logo" /></a></li>
+          <li><a href="#" data-label="Settings"><img class="logos" src="/ajustes.png" alt="Logo" /></a></li>
         </ul>
       </nav>
     </aside>
